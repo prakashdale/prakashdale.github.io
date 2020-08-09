71c7e664d7e26cbd94d496a83d0b4d6a7d64dd4f
Prakash Dale <prakashdale@gmail.com>
2020-08-09 17:11:12 +0530

    gist

--- src/opencv/02_read_images.py
+++ src/opencv/02_read_images.py
@@ -3,16 +3,12 @@ import numpy as np
 img = cv2.imread('lena.jpg', cv2.IMREAD_COLOR)
 
 if type(img) is np.ndarray:
-    # cv2.startWindowThread()
-    # cv2.namedWindow("lena")
     cv2.imshow('lena', img)
-    # cv2.waitKey(2000) #wait for 2 sec before closing window
-    k = cv2.waitKey() & 0xFF
-    
-
+    k = cv2.waitKey(1) & 0xFF
     if k == 27: # escape key
         cv2.destroyAllWindows()
     elif k == ord('s'):
+        # Save image as lena_copy.png
         cv2.imwrite('lena_copy.png', img)
         cv2.destroyAllWindows()
 else:
